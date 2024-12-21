a214e97230d67ab3e7818b2aa25f1521985f1243
Steven Chang <zminsc@seas.upenn.edu>
2024-12-21 16:07:11 -0500

    Initial commit

---
+++ Etho/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Etho
+//
+//  Created by Steven Chang on 12/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Etho/EthoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EthoApp.swift
+//  Etho
+//
+//  Created by Steven Chang on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct EthoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
